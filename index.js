5dece69ce40bd3fab9dbddf573c8963c26b1bf69
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-29 11:15:05 -0300

    get:receive parameters

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (require, resolve) => {
-  resolve.send('Hello my server with express.');
+app.get('/', (req, res) => {
+  res.send('Hello my server with express.');
 });
 
-app.get('/new-route', (require, resolve) => {
-  resolve.send('Hello, I am a new route.');
+app.get('/new-route', (req, res) => {
+  res.send('Hello, I am a new route.');
 });
 
-app.get('/products', (require, resolve) => {
-  resolve.json({
-    name: 'Product 1',
-    price: 1000
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000
+  });
+})
+
+app.get('/categories/:categoryId/:products/:productId', (req, res) =>{
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
