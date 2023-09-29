50c88cafa10e8043ce781a0cb679358eed355ced
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-29 19:40:12 -0300

    PUT,PATCH and DELETE

--- routes/users.router.js
+++ routes/users.router.js
@@ -25,4 +25,30 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 module.exports = router;
