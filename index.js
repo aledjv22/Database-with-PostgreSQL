b630a232d66a98c69f6a4acaffe2840e45e6cf2b
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-10-02 18:30:09 -0300

    origin update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:5500', 'http://127.0.0.1:5500'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)){
+    if (whitelist.includes(origin) || !origin){
       callback(null, true);
     } else {
       callback(new Error('Not allowed'));
