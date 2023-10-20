d4e6631282bebc01f5eb7638a464ca66f67a3fca
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-10-20 10:12:04 -0300

    one to one relationships

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
+const customersRouter = require('./customers.router');
 
 function routerApi (app){
   const router = express.Router();
@@ -10,6 +11,7 @@ function routerApi (app){
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
