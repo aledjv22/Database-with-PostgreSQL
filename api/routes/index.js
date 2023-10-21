6839f5225d6ca82c2f081321199c343b7ac68c32
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-10-21 14:56:48 -0300

    purchase order creation

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
+const ordersRouter = require('./orders.router');
 const customersRouter = require('./customers.router');
 
 function routerApi (app){
@@ -11,6 +12,7 @@ function routerApi (app){
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/orders', ordersRouter);
   router.use('/customers', customersRouter);
 }
 
---
+++ api/services/order.service.js
@@ -0,0 +1,43 @@
+const boom = require('@hapi/boom');
+
+const { models } = require('../libs/sequelize');
+
+class OrderService {
+  async create(data) {
+    const newOrder = await models.Order.create(data);
+    
+    return newOrder;
+  }
+
+  async find() {
+    const rta = await models.Order.findAll({
+      include: ['customer']
+    });
+
+    return rta;
+  }
+
+  async findOne(id) {
+    const order = await models.Order.findByPk(id, {
+      include: [{
+        association: 'customer',
+        include: ['user']
+      }]
+    });
+
+    return order;
+  }
+
+  async update(id, changes) {
+    return {
+      id,
+      changes
+    }
+  }
+
+  async delete(id) {
+    return { id };
+  }
+}
+
+module.exports = OrderService;
